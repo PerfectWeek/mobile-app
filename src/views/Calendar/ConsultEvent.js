f14f11066618041a8b2127baf4c7b8fb474f9ae9
Pierre SAID <pierre.said@epitech.eu>
2019-01-13 19:12:25 -0500

    Rework consult event style

--- src/views/Calendar/ConsultEvent.js
+++ src/views/Calendar/ConsultEvent.js
@@ -21,10 +21,10 @@ export class _ConsultEvent extends React.Component {
             EventTitle: '',
             description: '',
             localisation: '',
-            dateBeginEvent : '',
-            dateEndEvent : '',
-            beginTime : '',
-            endTime : '',
+            dateBeginEvent: '',
+            dateEndEvent: '',
+            beginTime: '',
+            endTime: '',
             recievedEvent: false
         };
         this.props.GetEventInfo(this.props.navigation.state.params.eventId)
@@ -47,12 +47,12 @@ export class _ConsultEvent extends React.Component {
         })
     }
 
+    componentDidUpdate() {
+        if (this.props.calendar && this.props.calendar.status === CalendarActionType.GetEventInfoSuccess && this.state.recievedEvent === false)
+            this.fillInfoEvent(this.props.calendar.event);
+    }
+
     render() {
-        // if (this.props.calendar && this.props.calendar.status === CalendarActionType.ModifyEventSuccess)
-        // {
-        //     this.props.navigation.goBack();
-        //     this.props.RefreshCalendar();
-        // }
 
         if (this.props.calendar && (this.props.calendar.status === CalendarActionType.ModifyEvent
             || this.props.calendar.status === CalendarActionType.GetEventInfo)
@@ -63,11 +63,10 @@ export class _ConsultEvent extends React.Component {
                     justifyContent: 'center',
                     alignItems: 'center'
                 }}>
-                    <Loader />
+                    <Loader/>
                 </Container>
             );
-        if (this.props.calendar && this.props.calendar.status === CalendarActionType.GetEventInfoSuccess && this.state.recievedEvent === false)
-            this.fillInfoEvent(this.props.calendar.event);
+
         return (
             <Container>
                 <View style={{
@@ -77,87 +76,110 @@ export class _ConsultEvent extends React.Component {
                         marginLeft: 10, marginRight: 30, flexGrow: 3
                     }}>
                         <Item>
-                            <Text style={{textAlign: 'center', color: 'black', fontFamily: 'Lato_Bold', fontSize: 26}} >
+                            <Text style={{margin:10, color: 'black', fontFamily: 'Lato_Medium', fontSize: 26}}
+                                  placeholder="Event name">
                                 {this.state.EventTitle}
                             </Text>
                         </Item>
                         <Item>
-                            <Text style={{textAlign: 'center', color: 'black', fontFamily: 'Lato_Medium', fontSize: 16}}>
+                            <Icon type='SimpleLineIcons' active name='pencil'/>
+                            <Text style={{margin:10, color: 'black', fontFamily: 'Lato_Medium', fontSize: 16}}
+                                  placeholder="Description">
                                 {this.state.description}
                             </Text>
                         </Item>
                         <Item>
-                            <Text style={{textAlign: 'center', color: 'black', fontFamily: 'Lato_Medium', fontSize: 16}}>
+                            <Icon type='SimpleLineIcons' active name='location-pin'/>
+                            <Text style={{margin:10, color: 'black', fontFamily: 'Lato_Medium', fontSize: 16}}
+                                   placeholder="Localisation">
                                 {this.state.localisation}
                             </Text>
                         </Item>
-                        <Title style={{
-                            color: 'black',
-                            fontFamily: 'Lato_Bold',
-                            fontSize: 18,
-                            marginTop: 20,
-                            flexDirection: 'row',
-                            justifyContent: 'flex-start',
-                        }}>End event</Title>
-                        <Item style={{ justifyContent: 'center',
-                            alignItems: 'center'}}>
-                            <DatePicker
-                                style={{width: 50}}
-                                date={this.state.beginTime}
-                                mode="time"
-                                format="HH:mm"
-                                confirmBtnText="Confirm"
-                                cancelBtnText="Cancel"
-                                minuteInterval={1}
-                                showIcon={false}
-                                onDateChange={(time) => {this.setState({beginTime: time});}}
-                            />
-                            <DatePicker
-                                style={{width: 200, height:50,  justifyContent: 'center',
-                                    alignItems: 'center'}}
-                                placeholder={this.state.dateBeginEvent}
-                                format="YYYY-MM-DD"
-                                minDate="2018-01-01"
-                                maxDate="2022-01-01"
-                                confirmBtnText="Confirm"
-                                cancelBtnText="Cancel"
-                                showIcon={false}
-                                onDateChange={(date) => {this.setState({dateBeginEvent: date})}}
-                            />
-                        </Item>
-                        <Title style={{
-                            color: 'black',
-                            fontFamily: 'Lato_Bold',
-                            fontSize: 18,
-                            marginTop: 20,
-                            flexDirection: 'row',
-                            justifyContent: 'flex-start'
-                        }}>Begin event</Title>
-                        <Item style={{ justifyContent: 'center',
-                            alignItems: 'center'}}>
-                            <DatePicker
-                                style={{width: 50}}
-                                date={this.state.endTime}
-                                mode="time"
-                                format="HH:mm"
-                                confirmBtnText="Confirm"
-                                cancelBtnText="Cancel"
-                                minuteInterval={1}
-                                showIcon={false}
-                                onDateChange={(time) => {this.setState({endTime: time});}}
-                            />
-                            <DatePicker
-                                style={{width: 200, height:50,  justifyContent: 'center',
-                                    alignItems: 'center', borderLeftColor: 'white'}}
-                                placeholder={this.state.dateEndEvent}
-                                format="YYYY-MM-DD"
-                                minDate="2018-01-01"
-                                maxDate="2022-01-01"
-                                confirmBtnText="Confirm"
-                                cancelBtnText="Cancel"
-                                showIcon={false}
-                                onDateChange={(date) => {this.setState({dateEndEvent: date})}}
-                            />
+
+                        <Item>
+                            <Icon style={{alignSelf: 'flex-start', marginTop: 10}} type='SimpleLineIcons' active
+                                  name='clock'/>
+
+                            <View style={{flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center'}}>
+
+                                <View style={{
+                                    flexDirection: 'row',
+                                    justifyContent: 'flex-start',
+                                    alignItems: 'center'
+                                }}>
+                                    <DatePicker
+                                        disabled
+                                        customStyles={{placeholderText: {color: 'black', fontFamily: 'Lato_Medium'}}}
+                                        style={{
+                                            width: 200, height: 50, justifyContent: 'center',
+                                            alignItems: 'center'
+                                        }}
+                                        placeholder={this.state.dateBeginEvent === '' ? "Beginning" : this.state.dateBeginEvent}
+                                        format="YYYY-MM-DD"
+                                        minDate="2018-01-01"
+                                        maxDate={this.state.dateEndEvent === '' ? "2022-01-01" : this.state.dateEndEvent}
+                                        confirmBtnText="Confirm"
+                                        cancelBtnText="Cancel"
+                                        showIcon={false}
+                                        onDateChange={(date) => {
+                                            this.setState({dateBeginEvent: date})
+                                        }}
+                                    />
+                                    <DatePicker disabled
+                                        customStyles={{placeholderText: {color: 'black', fontFamily: 'Lato_Medium'}}}
+                                        style={{width: 80}}
+                                        date={this.state.beginTime}
+                                        placeholder="End Time"
+                                        mode="time"
+                                        format="HH:mm"
+                                        confirmBtnText="Confirm"
+                                        cancelBtnText="Cancel"
+                                        minuteInterval={1}
+                                        showIcon={false}
+                                        onDateChange={(time) => {
+                                            this.setState({beginTime: time});
+                                        }}
+                                    />
+                                </View>
+                                <View style={{
+                                    flexDirection: 'row',
+                                    justifyContent: 'flex-start',
+                                    alignItems: 'center'
+                                }}>
+                                    <DatePicker disabled
+                                        customStyles={{placeholderText: {color: 'black', fontFamily: 'Lato_Medium'}}}
+                                        style={{
+                                            width: 200, height: 50, justifyContent: 'center',
+                                            alignItems: 'center', borderLeftColor: 'white'
+                                        }}
+                                        placeholder={this.state.dateEndEvent === '' ? "Ending" : this.state.dateEndEvent}
+                                        format="YYYY-MM-DD"
+                                        minDate={this.state.dateBeginEvent === '' ? "2018-01-01" : this.state.dateBeginEvent}
+                                        maxDate="2022-01-01"
+                                        confirmBtnText="Confirm"
+                                        cancelBtnText="Cancel"
+                                        showIcon={false}
+                                        onDateChange={(date) => {
+                                            this.setState({dateEndEvent: date})
+                                        }}
+                                    />
+                                    <DatePicker disabled
+                                        customStyles={{placeholderText: {color: 'black', fontFamily: 'Lato_Medium'}}}
+                                        style={{width: 80}}
+                                        date={this.state.endTime}
+                                        placeholder="End Time"
+                                        mode="time"
+                                        format="HH:mm"
+                                        confirmBtnText="Confirm"
+                                        cancelBtnText="Cancel"
+                                        minuteInterval={1}
+                                        showIcon={false}
+                                        onDateChange={(time) => {
+                                            this.setState({endTime: time});
+                                        }}
+                                    />
+                                </View>
+                            </View>
                         </Item>
                     </Form>
                 </View>
