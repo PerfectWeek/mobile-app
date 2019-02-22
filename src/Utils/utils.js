3f88ccbbbdf24839143b92065f5e6ae1a6f666ea
Pierre SAID <pierre.said@epitech.eu>
2019-02-21 19:53:28 -0500

    Added utils function
    - added deep object merge
    - added array to object
    - removed test redux

--- src/Utils/utils.js
+++ src/Utils/utils.js
@@ -1,7 +1,7 @@
 export function validateEmail(email) {
     if (email === '')
         return true;
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
 
@@ -15,10 +15,87 @@ export function validateNotEmpty(str) {
 export function validatePassword(psw) {
     if (psw === '')
         return true;
-    var re = /^[a-zA-Z0-9_-]{2,31}$/;
+    const re = /^[a-zA-Z0-9_-]{2,31}$/;
     return re.test(String(psw).toLowerCase());
 }
 
 export function comparePasswords(pwd1, pwd2) {
     return pwd1 === pwd2;
 }
+
+
+export const arrayToObject = (arr, keyField) =>
+    Object.assign({}, ...arr.map(item => ({[item[keyField]]: item})));
+
+
+// Deep merge
+function isMergeableObject(val) {
+    const nonNullObject = val && typeof val === 'object';
+
+    return nonNullObject
+        && Object.prototype.toString.call(val) !== '[object RegExp]'
+        && Object.prototype.toString.call(val) !== '[object Date]'
+}
+
+function emptyTarget(val) {
+    return Array.isArray(val) ? [] : {}
+}
+
+function cloneIfNecessary(value, optionsArgument) {
+    const clone = optionsArgument && optionsArgument.clone === true;
+    return (clone && isMergeableObject(value)) ? deepmerge(emptyTarget(value), value, optionsArgument) : value
+}
+
+function defaultArrayMerge(target, source, optionsArgument) {
+    const destination = target.slice();
+    source.forEach(function (e, i) {
+        if (typeof destination[i] === 'undefined') {
+            destination[i] = cloneIfNecessary(e, optionsArgument)
+        } else if (isMergeableObject(e)) {
+            destination[i] = deepmerge(target[i], e, optionsArgument)
+        } else if (target.indexOf(e) === -1) {
+            destination.push(cloneIfNecessary(e, optionsArgument))
+        }
+    });
+    return destination
+}
+
+function mergeObject(target, source, optionsArgument) {
+    const destination = {};
+    if (isMergeableObject(target)) {
+        Object.keys(target).forEach(function (key) {
+            destination[key] = cloneIfNecessary(target[key], optionsArgument)
+        })
+    }
+    Object.keys(source).forEach(function (key) {
+        if (!isMergeableObject(source[key]) || !target[key]) {
+            destination[key] = cloneIfNecessary(source[key], optionsArgument)
+        } else {
+            destination[key] = deepmerge(target[key], source[key], optionsArgument)
+        }
+    });
+    return destination
+}
+
+export function deepmerge(target, source, optionsArgument) {
+    const array = Array.isArray(source);
+    const options = optionsArgument || {arrayMerge: defaultArrayMerge};
+    const arrayMerge = options.arrayMerge || defaultArrayMerge;
+
+    if (array) {
+        return Array.isArray(target) ? arrayMerge(target, source, optionsArgument) : cloneIfNecessary(source, optionsArgument)
+    } else {
+        return mergeObject(target, source, optionsArgument)
+    }
+}
+
+deepmerge.all = function deepmergeAll(array, optionsArgument) {
+    if (!Array.isArray(array) || array.length < 2) {
+        throw new Error('first argument should be an array with at least two elements')
+    }
+
+    // we are sure there are at least 2 values, so it is safe to have no initial value
+    return array.reduce(function (prev, next) {
+        return deepmerge(prev, next, optionsArgument)
+    })
+};
