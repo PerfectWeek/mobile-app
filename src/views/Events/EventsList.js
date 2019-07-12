15eddfdbae8b533cd248976b3e9316aa3b288a07
Pierre SAID <pierre.said@epitech.eu>
2019-07-12 12:37:20 +0200

    fix Events pagination

--- src/views/Events/EventsList.js
+++ src/views/Events/EventsList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Dimensions, Platform, View, ScrollView, RefreshControl} from 'react-native';
+import { Dimensions, Platform, View, ScrollView, RefreshControl } from 'react-native';
 import {
     Header,
     Body,
@@ -7,16 +7,12 @@ import {
     Text, Button
 } from 'native-base';
 import connect from "react-redux/es/connect/connect";
-import {HeaderBackgroundColor, ScreenBackgroundColor} from "../../../Style/Constant";
+import { HeaderBackgroundColor, ScreenBackgroundColor } from "../../../Style/Constant";
 import Loader from "../../Components/Loader";
 import EventCard from "../../Components/EventCard";
-import {GetEventRecommendation} from "../../redux/Events/events.actions";
+import { GetEventRecommendation } from "../../redux/Events/events.actions";
 
 export class _EventsList extends React.Component {
-    static navigationOptions = {
-        header: null
-    };
-
     constructor(props) {
         super(props);
         this.props.GetEventRecommendation("2019-05-10T12:12:12", "2019-06-10T12:12:12", 10);
@@ -30,44 +26,34 @@ export class _EventsList extends React.Component {
     render() {
         if (this.props.events === undefined || this.props.loading !== false)
             return (
-                <View style={{flex: 1, flexDirection: 'row', alignItems: 'center'}}>
-                    <Loader/>
+                <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center' }}>
+                    <Loader />
                 </View>
             );
         return (
-            <View style={{backgroundColor: 'white'}}>
-                <Header androidStatusBarColor="#00AE93" style={{backgroundColor: HeaderBackgroundColor}}>
-                    <Body>
-                        <Title style={{color: 'black'}}>Events</Title>
-                    </Body>
-                </Header>
-            <ScrollView style={{backgroundColor: 'white'}}
-                        refreshControl={
-                            <RefreshControl
-                                refreshing={this.props.loading}
-                                onRefresh={this._onRefresh}
-                            />}
-                        contentContainerStyle={{
-                            flexGrow: 1,
-                            paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight,
-
-                        }}>
+            <ScrollView style={{ backgroundColor: 'white' }}
+                refreshControl={
+                    <RefreshControl
+                        refreshing={this.props.loading}
+                        onRefresh={this._onRefresh}
+                    />}
+                contentContainerStyle={{
+                    flexGrow: 1
+                }}>
                 {
                     Object.values(this.props.events).length === 0 ?
                         <View>
-                            <Text style={{marginTop: 20, textAlign: 'center', fontSize: 22}}>
+                            <Text style={{ marginTop: 20, textAlign: 'center', fontSize: 22 }}>
                                 You have no suggestion at the moment
                             </Text>
                         </View>
                         :
                         Object.values(this.props.events).map((event, index) => {
                             return <EventCard navigation={this.props.navigation} event={this.props.events[event.id]}
-                                              key={event.id}/>
+                                key={event.id} />
                         })
                 }
-
             </ScrollView>
-            </View>
         )
     }
 }
