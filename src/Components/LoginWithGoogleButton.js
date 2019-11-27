38d5d2e9f79df75e3d3968bac15d418da5fa17d0
pierre said <pierre.said@epitech.eu>
2019-11-27 20:13:57 +0100

    Google login Expo method

--- src/Components/LoginWithGoogleButton.js
+++ src/Components/LoginWithGoogleButton.js
@@ -3,10 +3,11 @@ import { Button } from "native-base";
 import { withNavigation } from "react-navigation";
 import { connect } from "react-redux";
 import { LoginGoogle } from "../redux/Login/login.actions";
-import { Google, AuthSession } from "expo";
+import { AuthSession } from "expo";
 // import * as Google from 'expo-google-app-auth';
 // import * as GoogleSignIn from 'expo-google-sign-in';
 import axios from "react-native-axios";
+import * as Google from "expo-google-app-auth";
 
 import {
   ShowErrorNotification,
@@ -35,44 +36,46 @@ class _LoginWithGoogleButton extends Component {
 
       console.log("BEFORE");
 
-      // let res = await Google.logInAsync({
-      //     // iosClientId: '801287294023-633lrp09bt9iglu4nkcld3vad2ivj69p.apps.googleusercontent.com',
-      //     // iosStandaloneAppClientId: '801287294023-633lrp09bt9iglu4nkcld3vad2ivj69p.apps.googleusercontent.com',
-      //     androidClientId: '802613708270-m496pmo592dflj044c4hconf4deo56rh.apps.googleusercontent.com',
-      //     androidStandaloneAppClientId: "802613708270-opv56tc6bha7frmet2gn8t54haep8rg0.apps.googleusercontent.com",
-      //     scopes: [
-      //         "https://www.googleapis.com/auth/userinfo.email",
-      //         "https://www.googleapis.com/auth/userinfo.profile",
-      //         "https://www.googleapis.com/auth/calendar.readonly",
-      //       ]
-      // });
-      const googleWebAppId =
-        "802613708270-0oi0ub3g8qpvjq15ai92orhj6qk4o12m.apps.googleusercontent.com";
-      const redirectUrl = AuthSession.getRedirectUrl();
-      console.log("redirectUrl : ", redirectUrl);
+      let res = await Google.logInAsync({
+        iosClientId:
+          "802613708270-g0fk0pqpke3m62fse0nt4qigppd5oje4.apps.googleusercontent.com",
+        iosStandaloneAppClientId:
+          "802613708270-kat25t9kln35o64cf8b031jjuekmi0of.apps.googleusercontent.com",
+        androidClientId:
+          "802613708270-m496pmo592dflj044c4hconf4deo56rh.apps.googleusercontent.com",
+        androidStandaloneAppClientId:
+          "802613708270-opv56tc6bha7frmet2gn8t54haep8rg0.apps.googleusercontent.com",
+        scopes: [
+          "https://www.googleapis.com/auth/userinfo.email",
+          "https://www.googleapis.com/auth/userinfo.profile",
+          "https://www.googleapis.com/auth/calendar.readonly"
+        ]
+      });
+      // const googleWebAppId =
+      //   "802613708270-0oi0ub3g8qpvjq15ai92orhj6qk4o12m.apps.googleusercontent.com";
+      // const redirectUrl = AuthSession.getRedirectUrl();
+      // console.log("redirectUrl : ", redirectUrl);
       // alert(redirectUrl);
 
-      let res = await AuthSession.startAsync({
-        authUrl:
-          `https://accounts.google.com/o/oauth2/v2/auth?` +
-          `&client_id=${googleWebAppId}` +
-          `&redirect_uri=${encodeURIComponent(redirectUrl)}` +
-          `&response_type=code` +
-          `&access_type=offline` +
-          `&scope=${encodeURIComponent('profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar.readonly')}`
-      });
+      // let res = await AuthSession.startAsync({
+      //   authUrl:
+      //     `https://accounts.google.com/o/oauth2/v2/auth?` +
+      //     `&client_id=${googleWebAppId}` +
+      //     `&redirect_uri=${encodeURIComponent(redirectUrl)}` +
+      //     `&response_type=code` +
+      //     `&access_type=offline` +
+      //     `&scope=${encodeURIComponent('profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar.readonly')}`
+      // });
 
-        const google_res = await axios.post("https://www.googleapis.com/oauth2/v4/token", {
-            code: res.params.code,
-            client_id: googleWebAppId,
-            client_secret: "v-G_2ONgX5PWWVPthmnVLlsS",
-            redirect_uri: redirectUrl,
-            grant_type: 'authorization_code'
-        });
-        console.log("aloha :", google_res);    
-      
-      
-      
+      // const google_res = await axios.post("https://www.googleapis.com/oauth2/v4/token", {
+      //     code: res.params.code,
+      //     client_id: googleWebAppId,
+      //     client_secret: "v-G_2ONgX5PWWVPthmnVLlsS",
+      //     redirect_uri: redirectUrl,
+      //     grant_type: 'authorization_code'
+      // });
+      // console.log("aloha :", google_res);
+
       // let res = await Google.logInAsync({
       //     iosClientId: '801287294023-633lrp09bt9iglu4nkcld3vad2ivj69p.apps.googleusercontent.com',
       //     iosStandaloneAppClientId: '801287294023-633lrp09bt9iglu4nkcld3vad2ivj69p.apps.googleusercontent.com',
@@ -85,12 +88,20 @@ class _LoginWithGoogleButton extends Component {
       // const { type, user } = await GoogleSignIn.signInAsync();
       console.log("res : ", res);
 
-      if (google_res.status === 200) {
-          const auth = await ProviderService.ConnectWithGoogleTokens(google_res.data.access_token, google_res.data.refresh_token);
-          console.log("AUTH : ", auth);
+      if (res.type === "success") {
+        const auth = await ProviderService.ConnectWithGoogleTokens(
+          res.accessToken,
+          res.refreshToken
+        );
+        console.log("AUTH : ", auth);
 
-          this.props.LoginGoogle(auth.user.email, auth.token, auth.user.name, auth.user.id);
-          // await ShowSuccessNotification( i18n.t('login.success') + ` ${auth.user.name}!`);
+        this.props.LoginGoogle(
+          auth.user.email,
+          auth.token,
+          auth.user.name,
+          auth.user.id
+        );
+        // await ShowSuccessNotification( i18n.t('login.success') + ` ${auth.user.name}!`);
       }
     } catch (e) {
       await ShowErrorNotification(i18n.t("login.cofail") + " Google");
